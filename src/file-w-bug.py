ff71576e670a9fd3de5f422a78b77fa5f9fa0c6d
Arturo Palau <132499885+arturo-palau-sonarsource@users.noreply.github.com>
2024-03-27 10:57:52 -0500

    Update file-w-bug.py

--- src/file-w-bug.py
+++ src/file-w-bug.py
@@ -3,3 +3,7 @@ print('this should have issues')
 def noncompliant():
     ls = [1, 2, 3]
     foo(ls[3])  # Noncompliant
+
+
+from mysql.connector import connection
+connection.MySQLConnection(host='localhost', user='sonarsource', password='')  # Noncompliant
